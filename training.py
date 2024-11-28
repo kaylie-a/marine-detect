c195d20956b09c924e607ac9d1056e138737b0cf
kaylie-a <kaguila@uccs.edu>
2024-11-28 08:29:37 -0700

    Retrained clownfish and finish training fish

--- training.py
+++ training.py
@@ -4,7 +4,7 @@ import os
 current_dir = os.getcwd()
 
 # Load a pretrained model (optional)
-model = YOLO() 
+model = YOLO(current_dir+"\\models\\clownfish_model.pt")
 
 # Train the model on your custom dataset
 model.train(data=current_dir+"\\datasets\\Images\\Fish\\(Clownfish)-Amphiprion-percula-.v3i.yolov5pytorch\\data.yaml", epochs=50)
